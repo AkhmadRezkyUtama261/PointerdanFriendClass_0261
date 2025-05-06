ae0a14563f9c49804988ca8e5550549b63621347
AkhmadRezkyUtama261 <ahkmadrezkyutama369@gmail.com>
2025-05-06 10:34:21 +0700

    Menambahkan akses modifier dan menambahkan vod input

---
+++ contoh.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class PersegiPanjang
+{
+public: //akses modifier
+int panjang, lebar;
+
+public:
+void input ()
+{
+cout << "Masukkan Panjang : ";
+cin >> panjang;
+cout << "masukkan lebar : ";
+cin >> lebar;
+}
+int luasPp(int a, int b)
+{return a * b;}
+
+Lingkaran ol;
+
+
+
+
+}
