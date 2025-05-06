f82dac1188347ba44b5bb8f78e406ac382669efc
AkhmadRezkyUtama261 <ahkmadrezkyutama369@gmail.com>
2025-05-06 11:45:51 +0700

    TERAKHIR

--- FriendClassSatu.cpp
+++ FriendClassSatu.cpp
@@ -9,6 +9,37 @@ private:
     string nama;
 
 public:
-    friend void setNama(string pNama);
+    void setNama(string pNama);
     friend class siswa;
 };
+
+class siswa
+{
+private:
+int id;
+
+public:
+void setId(int pId);
+void displayAll(orang &a);
+};
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa ::setId(int pId)
+{
+    id = pId;
+}
+int main(){
+orang joko;
+joko.setNama("Joko susilo");
+siswa joko_siswa;
+joko_siswa.setId(1);
+joko_siswa.displayAll(joko);
+return 0;
+}
